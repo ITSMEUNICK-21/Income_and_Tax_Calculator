64db952c7b34ca1d4724cf4017b838a4bb94f568
HEGDE NIKHIL SURESH <nikhilsureshhegde21@gmail.com>
2022-04-02 22:59:57 +0530

    Delete 3_Implementation/documentation/html directory

--- 3_Implementation/documentation/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['documentation_5fmain_0',['documentation_main',['../md__c___users_nikhil_projects__m1__income_and__tax__calculator_3__implementation_inc_documentation_main.html',1,'']]]
-];
--- 3_Implementation/documentation/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['all_5ffunctions_2eh_0',['all_functions.h',['../all__functions_8h.html',1,'']]]
-];
