28b46cac3877fb7c26ff3e269c5decbe5d8a11fe
ITSMEUNICK-21 <nikhilsureshhegde21@gmail.com>
2022-04-02 22:38:48 +0530

    Add Doxygen Documentation

---
+++ 3_Implementation/documentation/html/search/all_2.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['newreg_5f10_5fto_5f12andhalflpa_5fall_0',['newreg_10_to_12andhalflpa_all',['../all__functions_8h.html#a7ee78ec313c32912c5a3b28aae31eaee',1,'all_functions.h']]],
+  ['newreg_5f12andhalf_5fto_5f15lpa_5fall_1',['newreg_12andhalf_to_15lpa_all',['../all__functions_8h.html#a6e6b18a2aba4cc8d5ac5c85b992868b0',1,'all_functions.h']]],
+  ['newreg_5f2andhalf_5fto_5f5lpa_5fall_2',['newreg_2andhalf_to_5lpa_all',['../all__functions_8h.html#ad9e9656b30ce87a4cd88f882b480c3f4',1,'all_functions.h']]],
+  ['newreg_5f5_5fto_5f7andhalflpa_5fall_3',['newreg_5_to_7andhalflpa_all',['../all__functions_8h.html#a7814c9226d3aae25545063a0be574001',1,'all_functions.h']]],
+  ['newreg_5f7andhalf_5fto_5f10lpa_5fall_4',['newreg_7andhalf_to_10lpa_all',['../all__functions_8h.html#ad827fb061a598866a5c411be56789f17',1,'all_functions.h']]],
+  ['newreg_5fabove15lpa_5fall_5',['newreg_above15lpa_all',['../all__functions_8h.html#a099a0758c6b8f43c7841f40e438eb167',1,'all_functions.h']]],
+  ['newreg_5fall_6',['newreg_all',['../all__functions_8h.html#ab9ae56f2887ba2147b75fbf3678b518e',1,'all_functions.h']]]
+];
---
+++ 3_Implementation/documentation/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['documentation_5fmain_0',['documentation_main',['../md__c___users_nikhil_projects__m1__income_and__tax__calculator_3__implementation_inc_documentation_main.html',1,'']]]
+];
